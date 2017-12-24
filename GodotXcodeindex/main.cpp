6cea234503d409d66a74c00f7d41337c205f8b2b
elizeu <elizeu@insoniastudios.com>
2017-12-23 22:31:15 -0200

    xcode project

---
+++ GodotXcodeindex/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GodotXcodeindex
+//
+//  Created by Elizeu Nogueira da Rosa Jr on 03/12/17.
+//  Copyright © 2017 Elizeu Nogueira da Rosa Jr. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
